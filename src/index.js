2cc9349afaa280cd8cb331e0a5a60e5146fe8a33
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-08-09 15:23:19 +0300

    +

--- src/index.js
+++ src/index.js
@@ -5,5 +5,27 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 
 //2. Доступ до ресурсів
-const input = document.querySelector("#search-box");
+const input = document.querySelector("#input");
 const DEBOUNCE_DELAY = 300;
+const btnSearch = document.querySelector("button[data-search]");
+
+// 3. Прослуховування інпута із затримкою!
+input.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY));
+
+// 4. Функції виведення помилок
+// 4.1 Введення недостатньої кількості символів
+function infoAlert() {
+    Notify.info("Too many matches found. Please enter a more specific name.");
+}
+
+// 4.2 Введення неіснуючої країни
+function wrongAlert() {
+    Notify.failure("Oops, there is no country with that name");
+}
+// 5. Функція trim для вирізки пробілів!
+function onInput() {
+    const name = input.value.trim();
+    if (name === "") {
+        return (countryList.innerHTML = ""), (countryInfo.innerHTML = "");
+    }
+}
