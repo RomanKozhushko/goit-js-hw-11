7d36cc72eaa0b92adf0c6aad2553ffa36e6bdbe3
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-08-09 14:02:27 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-1
+1//1. Імпорт бібліотек
+import debounce from 'lodash.debounce'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import './css/styles.css';
+import SimpleLightbox from "simplelightbox";
