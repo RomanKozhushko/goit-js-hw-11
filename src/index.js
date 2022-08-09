a29a1f374ccf211ec1a789d375957b842309e755
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-08-09 14:12:19 +0300

    !

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 1//1. Імпорт бібліотек
 import debounce from 'lodash.debounce'
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import './css/styles.css';
+// import './css/styles.css';
 import SimpleLightbox from "simplelightbox";
+
+//2. Доступ до ресурсів
+const input = document.querySelector("#search-box");
+const DEBOUNCE_DELAY = 300;
